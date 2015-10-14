a30884130278bb3d264a9e3215604ba733c68ac0
huangzihao <494269565@qq.com>
2015-10-14 15:49:25 +0800

    Update rujia_template.rb

--- rujia_template.rb
+++ rujia_template.rb
@@ -9,7 +9,6 @@ remove_file "Gemfile"
 run "touch Gemfile"
 add_source 'https://ruby.taobao.org'
 gem 'rails', '4.2.1'
-gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
